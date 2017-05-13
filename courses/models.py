cacef2207a0260e66838c03c968e97a4dffb4da8
René Fleschenberg <rene@fleschenberg.net>
2017-05-13 12:25:17 +0200

    After 2.1 (3.1 according to new numbering)

--- courses/models.py
+++ courses/models.py
@@ -1,9 +1,15 @@
 from django.core.urlresolvers import reverse
 from django.db import models
 
+from students.models import Student
+
 
 class Course(models.Model):
     name = models.CharField(max_length=200)
+    students = models.ManyToManyField(Student)
 
     def get_absolute_url(self):
         return reverse('course_detail', args=(self.id, ))
+
+    def __str__(self):
+        return self.name
