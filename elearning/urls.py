43f9f0a414e3432071ea0e79978c75da82190523
René Fleschenberg <rene@fleschenberg.net>
2017-05-13 17:26:55 +0100

    After 3.2 (new numbering)

--- elearning/urls.py
+++ elearning/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import url
 from django.contrib import admin
 
 from courses.views import course_add, course_detail, course_list
+from students.views import student_detail
 
 
 urlpatterns = [
@@ -24,5 +25,7 @@ urlpatterns = [
     url(r'^course_detail/(?P<course_id>\d)/$', course_detail,
         name='course_detail'),
     url(r'^course_add/$', course_add, name='course_add'),
+    url(r'^student_detail/(?P<student_id>\d)/$', student_detail,
+        name='student_detail'),
     url(r'^$', course_list),
 ]
