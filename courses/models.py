b2a35b1c89d7cfae6fb99c262db73dced60f2043
René Fleschenberg <rene@fleschenberg.net>
2017-05-14 16:26:57 +0100

    After 3.4

--- courses/models.py
+++ courses/models.py
@@ -1,4 +1,5 @@
 from django.core.urlresolvers import reverse
+from django.conf import settings
 from django.db import models
 
 from students.models import User
@@ -13,3 +14,42 @@ class Course(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Section(models.Model):
+    course = models.ForeignKey(Course)
+    title = models.CharField(max_length=100)
+    number = models.IntegerField()
+    text = models.TextField()
+
+    class Meta:
+        unique_together = ('course', 'number', )
+
+    def __str__(self):
+        return self.title
+
+
+class Question(models.Model):
+    section = models.ForeignKey(Section)
+    text = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.text
+
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question)
+    text = models.CharField(max_length=1000)
+    correct = models.BooleanField()
+
+    def __str__(self):
+        return self.text
+
+
+class UserAnswer(models.Model):
+    question = models.ForeignKey(Question)
+    answer = models.ForeignKey(Answer)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+
+    class Meta:
+        unique_together = ('question', 'user',)
