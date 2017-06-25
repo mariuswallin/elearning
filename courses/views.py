3a3847ec2b6a237639d22fa7cc3152e36b75b7df
René Fleschenberg <rene@fleschenberg.net>
2017-06-25 20:13:00 +0200

    After 3.5 revision 1

--- courses/views.py
+++ courses/views.py
@@ -1,8 +1,11 @@
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.db import transaction
+from django.core.exceptions import PermissionDenied, SuspiciousOperation
+from django.core.urlresolvers import reverse
+from django.shortcuts import render, redirect
 
 from courses.forms import CourseForm
-from courses.models import Course
+from courses.models import Course, Section, Question, UserAnswer
 
 
 def course_detail(request, course_id):
@@ -29,4 +32,39 @@ def course_add(request):
         form = CourseForm()
     return render(request, 'courses/course_form.html', {
         'form': form,
+    })
+
+
+def do_section(request, section_id):
+    section = Section.objects.get(id=section_id)
+    return render(request, 'courses/do_section.html', {
+        'section': section,
+    })
+
+
+def do_test(request, section_id):
+    if not request.user.is_authenticated():
+        raise PermissionDenied
+    section = Section.objects.get(id=section_id)
+    if request.method == 'POST':
+        with transaction.atomic():
+            UserAnswer.objects.filter(user=request.user,
+                                      question__section=section).delete()
+            for key, value in request.POST.items():
+                if key == 'csrfmiddlewaretoken':
+                    continue
+                # {'question-1': '2'}
+                question_id = key.split('-')[1]
+                question = Question.objects.get(id=question_id)
+                answer_id = int(request.POST.get(key))
+                if answer_id not in question.answer_set.values_list('id', flat=True):
+                    raise SuspiciousOperation('Answer is not valid for this question')
+                user_answer = UserAnswer.objects.create(
+                    user=request.user,
+                    question=question,
+                    answer_id=answer_id,
+                )
+        return redirect(reverse('show_results', args=(section.id,)))
+    return render(request, 'courses/do_test.html', {
+        'section': section,
     })
